a46c9a40717ebd575e6318fb4c1c40f5fc23bb52
CleytonSouza <cleytonnac@gmail.com>
2017-06-02 20:58:00 -0300

    Configuration

--- resources/configs/configuration.php
+++ resources/configs/configuration.php
@@ -3,8 +3,8 @@ $app['debug'] = getenv('APPLICATION_DEBUG') == 'true' ?: false;
 
 $app['db.options'] = array(
     'driver'   => getenv('APPLICATION_DB_DRIVE') ? : 'pdo_mysql',
-    'dbname'   => getenv('APPLICATION_DB_DBNAME') ? : 'example',
+    'dbname'   => getenv('APPLICATION_DB_DBNAME') ? : 'bookApi',
     'host'     => getenv('APPLICATION_DB_HOST') ? : 'localhost',
     'user'     => getenv('APPLICATION_DB_USER') ? : 'root',
-    'password' => getenv('APPLICATION_DB_PASSWORD') ? : '123456789',
+    'password' => getenv('APPLICATION_DB_PASSWORD') ? : '4linux',
 );
