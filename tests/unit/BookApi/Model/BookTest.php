f6fa24a394b7ebbc0e03a55346be1e0a209f014a
Marcio Biason <marcio@servcredi.com.br>
2017-06-01 22:50:01 -0300

    Incluindo PHPUnit #5

---
+++ tests/unit/BookApi/Model/BookTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace BookApi\Model;
+
+class BookTest extends \PHPUnit\Framework\TestCase
+
+{
+    /**
+     * @var Sum
+     */
+    protected $object;
+    /**
+     * Sets up the fixture, for example, opens a network connection.
+     * This method is called before a test is executed.
+     */
+    protected function setUp()
+
+    {
+
+        $this->object = new Book();
+
+    }
+
+    /**
+     * Tears down the fixture, for example, closes a network connection.
+     * This method is called after a test is executed.
+     */
+    protected function tearDown()
+
+    {
+
+        unset($this->object);
+
+    }
+
+    /**
+     * @covers BookApi\Model\Book::getAuthors
+     */
+    public function getAuthors()
+
+    {
+        $authors = $this->object->getAuthors();
+
+        $this->assertInternalType("array",$authors);
+
+        $author = $authors[0];
+
+        $this->assertInstanceof("BookApi\Model\Author",$author);
+
+    }
+
+}
