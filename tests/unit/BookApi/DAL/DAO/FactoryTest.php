a3d3e9db9d7fe573f428786caf4f8de47dec0a1e
CleytonSouza <cleytonnac@gmail.com>
2017-06-02 22:46:23 -0300

    TestConexaoUnit

---
+++ tests/unit/BookApi/DAL/DAO/FactoryTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace BookApi\DAL\DAO;
+
+class FactoryTest extends \PHPUnit\Framework\TestCase
+{
+	/**
+     * @var Sum
+     */
+    protected $object;
+    /**
+     * Sets up the fixture, for example, opens a network connection.
+     * This method is called before a test is executed.
+     */
+    protected function setUp()
+    {
+        $this->object = new Factory();
+    }
+
+    /**
+     * Tears down the fixture, for example, closes a network connection.
+     * This method is called after a test is executed.
+     */
+    protected function tearDown()
+    {
+        unset($this->object);
+    }
+
+    public function testCreateConnection()
+    {
+    	$this->assertInstanceOf("\PDO",$this->object->createConnection());
+    }
+
+}
