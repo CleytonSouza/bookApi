3c2f31912ad83f1aea4476cc05570535da1f1070
sidjeanp <sidjeanp@gmail.com>
2017-06-05 22:30:44 -0300

    Iniciado arquivo de teste

--- tests/unit/BookApi/DAL/DAO/BookTest.php
+++ tests/unit/BookApi/DAL/DAO/BookTest.php
@@ -2,7 +2,102 @@
 
 namespace BookAPI\DAL\DAO;
 
+
 class BookTest extends \PHPUnit\Framework\TestCase
 {
+    /**
+     * @var Sum
+     */
     protected $object;
+
+    /**
+     * Sets up the fixture, for example, opens a network connection.
+     * This method is called before a test is executed.
+     */
+    protected function setUp()
+    {
+    	$this->object = new \BookApi\DAL\DAO\Book();
+    }
+
+    /**
+     * Tears down the fixture, for example, closes a network connection.
+     * This method is called after a test is executed.
+     */
+    protected function tearDown()
+    {
+        unset($this->object);
+    }
+
+
+    /**
+     * @covers Book::all
+     */
+    public function testAll()
+    {
+        $this->assertInternalType("array", $this->object->all()); 
+        $book = $this->object->all()[0];
+        $this->assertInstanceof("\\BookApi\\Model\\Book",$book);
+    }    
+
+    /**
+     * @covers Book::insert
+     */
+    public function testInsert()
+    {
+        //$this->assertInstanceof("BookApi\DAL\DAO\Book", $author);        
+    }
+
+    /**
+     * @covers Book::insert
+     */
+    public function testUpdate()
+    {
+        //$this->assertInstanceof("BookApi\DAL\DAO\Book", $author);        
+    }
+
+    /**
+     * @covers Book::insert
+     */
+    public function testDelete()
+    {
+        //$this->assertInstanceof("BookApi\DAL\DAO\Book", $author);        
+    }    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /**
+     * @covers Book::toArray
+     */
+    public function testToArray()
+    {
+        $this->assertInternalType("array", $this->object->toArray());
+        $this->assertArrayHasKey("isbn", $this->object->toArray());
+        $this->assertArrayHasKey("name", $this->object->toArray());
+    }
+
+    /**
+     * /@cover Book::getTitle
+     */
+    public function testGetTitle()
+    {
+        $this->assertInternalType('string', $this->object->getName());
+        $this->assertEquals('PHP Moderno: Novos recursos e boas práticas', $this->object->getName());
+    }
 }
+
